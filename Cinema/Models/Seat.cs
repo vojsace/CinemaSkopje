c8058164de3fd78ba9cd5f18451d2ee2854fb357
vojsace <vojsace@github.com>
2020-01-13 14:47:58 +0100

    Added Privacy Policy

---
+++ Cinema/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Models
+{
+    public class Seat
+    {
+        public int SeatId { get; set; }
+        public string SeatNo { get; set; }
+        public string SeatTotal { get; set; }
+
+        public ICollection<Movie> Movie { get; set; }
+    }
+}
